eb0288f436573618cd12f95f72f3ac8d94eb6430
Pedro G. Perez <pedrogperezalvarez@gmail.com>
2022-02-04 20:24:03 -0800

    Lab14UFrontEndUpdate1

--- src/Profile.js
+++ src/Profile.js
@@ -1,24 +1,37 @@
-import { Component } from "react";
-import { Redirect } from "react-router-dom";
+// import { Component } from "react";
+// import { Redirect } from "react-router-dom";
+
+// class Profile extends Component {
+
+//   render() {
+
+//     if (this.props.user) {
+
+//       return (
+//         <div>
+//           <h2>{this.props.user.username}</h2>
+//           <p>{this.props.user.email}</p>
+//         </div>
+//       );
+
+//     } else {
+
+//       return <Redirect to="/" />
+//     }
+//   }
+// };
+
+// export default Profile;
+
+import React, { Component } from 'react';
+import { withAuth0 } from '@auth0/auth0-react';
+// const user = { name: 'sara', email: 'sara@codefellows.com' };
 
 class Profile extends Component {
 
   render() {
-
-    if (this.props.user) {
-
-      return (
-        <div>
-          <h2>{this.props.user.username}</h2>
-          <p>{this.props.user.email}</p>
-        </div>
-      );
-
-    } else {
-
-      return <Redirect to="/" />
-    }
+    return <div>Hello {this.props.auth0.user.name}:{this.props.auth0.user.email}</div>;
   }
-};
+}
 
-export default Profile;
+export default withAuth0(Profile);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from "@auth0/auth0-react";
 
+// once you sign up for an Auth0 account, they will provide you with the domain and clientId
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
+    redirectUri={process.env.REACT_APP_AUTH_REDIRECT_URI}
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
